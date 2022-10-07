8725961edd2b03936e214d867f483cd87fc0c921
Rasika srimal <77890327+rasikasrimal@users.noreply.github.com>
2022-10-07 19:42:09 +0530

    Add files via upload

---
+++ PheonixAirline/src/java/com/phoenixairline/controllers/FlightControllers/UpdateFlightInfoServlet.java
@@ -0,0 +1,44 @@
+package com.phoenixairline.controllers.FlightControllers;
+
+import com.phoenixairline.models.Flight;
+import com.phoenixairline.models.FlightAccess;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UpdateFlightInfoServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public UpdateFlightInfoServlet() {
+        super();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        String FlightId = request.getParameter("flightId");
+        String Takeoff_airport = request.getParameter("takeoff_airport");
+        String Takeoff_time = request.getParameter("takeoff_time");
+        String Takeoff_date = request.getParameter("takeoff_date");
+        String Landing_airport = request.getParameter("landing_airport");
+        String Landing_time = request.getParameter("landing_time");
+        String Landing_date = request.getParameter("landing_date");
+        String Gate = request.getParameter("gate");
+        String cost = request.getParameter("cost");
+        String aircraftId = request.getParameter("aircraft_id");
+
+        Flight flightBean = new Flight(FlightId, Takeoff_airport, Takeoff_time, Takeoff_date, Landing_airport, Landing_time, Landing_date, Gate, cost, aircraftId);
+        FlightAccess flightAccess = new FlightAccess();
+
+        String result = flightAccess.UpdateFlightDetails(flightBean);
+        PrintWriter out = response.getWriter();
+        out.println(result);
+    }
+
+}
---
+++ PheonixAirline/src/java/com/phoenixairline/controllers/ticketmanagement/CreateTicketServlet.java
@@ -0,0 +1,70 @@
+package com.phoenixairline.controllers.ticketmanagement;
+
+import com.phoenixairline.models.Ticket;
+import com.phoenixairline.models.TicketAccess;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class CreateTicketServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+   
+        int ticketId = 1;
+        int flightId = Integer.parseInt(request.getParameter("flightId"));
+        System.out.println(flightId);
+        
+        //get flight details to form
+        List selectedValues = new ArrayList();
+        TicketAccess ticketaccess = new TicketAccess();
+        selectedValues = ticketaccess.getSelectedData(flightId);
+        request.setAttribute("selectedResult", selectedValues);
+        RequestDispatcher rd = request.getRequestDispatcher("AllFlightDetails.jsp");
+        rd.forward(request, response);
+        
+        
+        String classId = request.getParameter("class");
+        int seatId = Integer.parseInt(request.getParameter("seatNumber"));
+        int seats = Integer.parseInt(request.getParameter("seats"));//check with db date type
+        float price = Float.parseFloat(request.getParameter("flightPrice"));
+        switch (classId) {
+            case "101":
+                price = (float) (price * 1.9)*seats;
+                break;
+            case "102":
+                price = (float) (price * 1.5)*seats;
+                break;
+            default:
+                price = price*seats;
+                break;
+        }
+
+        HttpSession session = request.getSession();
+        int userId = (int) session.getAttribute("user_id");
+        System.out.println("user id from session " + userId);
+        
+        System.out.println("before send");
+        Ticket ticketBean = new Ticket(ticketId,price,userId,flightId,seatId,classId);
+        TicketAccess ticketAccess = new TicketAccess();
+
+        System.out.println("After send");
+        String result = ticketAccess.createTicket(ticketBean);
+        PrintWriter out = response.getWriter();
+        out.println(result);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
